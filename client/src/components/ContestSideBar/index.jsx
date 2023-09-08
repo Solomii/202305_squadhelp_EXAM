cb371ecaa6e2c5e1f28f09729cafe0bb4d95616a
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-09-08 14:56:14 +0300

    Fix imege in client

--- client/src/components/ContestSideBar/index.jsx
+++ client/src/components/ContestSideBar/index.jsx
@@ -66,7 +66,7 @@ const ContestSideBar = props => {
             <div className={styles.customerInfo}>
               <img
                 src={
-                  User.avatar === 'anon.png'
+                  User.avatar === null
                     ? CONSTANTS.ANONYM_IMAGE_PATH
                     : `${CONSTANTS.publicURL}${User.avatar}`
                 }
