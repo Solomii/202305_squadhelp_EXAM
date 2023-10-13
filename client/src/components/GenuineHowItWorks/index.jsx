787aa128149f2dccb686160f5a6896349c134666
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-10-13 16:03:50 +0300

    Add image SvgMobileImage GenuineHowItWorks

--- client/src/components/GenuineHowItWorks/index.jsx
+++ client/src/components/GenuineHowItWorks/index.jsx
@@ -4,31 +4,34 @@ import GenuineWaysToUse from './GenuineWaysToUse';
 import GenuineNamingContestsWork from './GenuineNaminingContestsWork';
 import GenuineStartContest from './GenuineStartContest';
 import GenuineLaunchingContest from './GenuineLaunchingContest';
-import SvgMobileImage from '../icons/SvgMobileImage'
+import SvgMobileImage from '../icons/SvgMobileImage';
 
 const GenuineHowItWorks = () => {
   return (
     <div className={styles.genuineBox}>
       <div className={styles.howDoesWork}>
         <div>
-          <span>World's #1 Naming Platform</span>
+          <div>
+            <span>World's #1 Naming Platform</span>
+          </div>
+          <div>
+            <h1>How Does Squadhelp Work?</h1>
+            <p>
+              Squadhelp helps you come up with a great name for your business by
+              combining the power of crowdsourcing with sophisticated technology
+              and Agency-level validation services.
+            </p>
+          </div>
+          <div>
+            <a className={styles.playButton} href='https://vimeo.com/826948811'>
+              <i class='fas fa-play '></i>
+              Play Video
+            </a>
+          </div>
         </div>
-        <div>
-          <h1>How Does Squadhelp Work?</h1>
-          <p>
-            Squadhelp helps you come up with a great name for your business by
-            combining the power of crowdsourcing with sophisticated technology
-            and Agency-level validation services.
-          </p>
-        </div>
-        <div>
-          <SvgMobileImage/>
-        </div>
-        <div>
-          <a className={styles.playButton} href='https://vimeo.com/826948811'>
-            <i class='fas fa-play '></i>
-            Play Video
-          </a>
+
+        <div className={styles.wrapperMobileImage}>
+          <SvgMobileImage />
         </div>
       </div>
       <div className={styles.genuineWaysBox}>
@@ -42,9 +45,8 @@ const GenuineHowItWorks = () => {
         </div>
       </div>
       {/* <GenuineNamingContestsWork /> */}
-      <GenuineLaunchingContest/>
-      <GenuineStartContest/>
-   
+      <GenuineLaunchingContest />
+      <GenuineStartContest />
 
       {/* the end genuine page */}
     </div>
